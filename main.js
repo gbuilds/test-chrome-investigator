a726178885e527b6049d99e29d40ea1d5cfd40fa
= <gbuilds@users.noreply.github.com>
2016-10-04 08:23:38 -0400

    console log that js loaded

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 var script = document.createElement("script");
 script.src = "https://gbuilds.github.io/investigator.js";
 document.head.appendChild(script)
+console.log("gs investigator.js loaded");
